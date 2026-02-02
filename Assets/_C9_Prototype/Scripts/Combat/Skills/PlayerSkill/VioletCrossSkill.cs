0ada5c6eea6814aefbdef0cc1c5bedf5d33fca8b
Apo <abdurrahman_usta_41@hotmail.com>
2026-02-03 01:04:26 +0300

    v0.20
    violetCross particle

--- Assets/_C9_Prototype/Scripts/Combat/Skills/PlayerSkill/VioletCrossSkill.cs
+++ Assets/_C9_Prototype/Scripts/Combat/Skills/PlayerSkill/VioletCrossSkill.cs
@@ -4,17 +4,33 @@ public class VioletCrossSkill : SkillBehaviour
 {
     [SerializeField] VioletCrossProjectile violetCrossProjectilePrefab;
     [SerializeField] Transform firePoint;
+    [SerializeField] ParticleSystem[] violetCrossParticle;
 
 
     public override void Execute()
     {
         var projectile = Instantiate(violetCrossProjectilePrefab, firePoint.position, firePoint.rotation);
-
+        StartShockWawe();
         projectile.Init(firePoint.forward, skillData.projectileSpeed, skillData.damage, skillData.skillRange);
     }
 
     public override void Stop()
     {
         //
+        StopShockWawe();
+    }
+    void StartShockWawe()
+    {
+        foreach (var item in violetCrossParticle)
+        {
+            item.Play();
+        }
+    }
+    void StopShockWawe()
+    {
+        foreach (var item in violetCrossParticle)
+        {
+            item.Stop(true, ParticleSystemStopBehavior.StopEmitting);
+        }
     }
 }
